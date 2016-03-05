45e5a470d4c5bba30f581eabee194d2dd702ff11
Gwendolyn Bischoff <gwen.bischoff@hotmail.com>
2016-03-05 09:02:59 +0100

    cart-session will be droped after checkout

--- order_exit.php
+++ order_exit.php
@@ -14,6 +14,7 @@
 		zip_to_db();
 
 		function into_db($query){
+			//Alle anfragen an die Datenbank werden über diese Funktion ausgeführt
 			require 'inc/configure.php.inc';
 			$sql->query($query);
 			$result = $sql->lastInsertId();
@@ -22,44 +23,55 @@
 		}
 
 		function person_data_to_db(){
+			// Fügt den Vornamen, Namen, Straße, Hausnr, PLZ zur Tabelle `person` in der DB hinzu
 			$prename = $_POST['prename'];
 			$name = $_POST['name'];
 			$street = $_POST['street'];
 			$streetnumber = $_POST['streetnumber'];
 			$zip = $_POST['zip'];
 			$person_id = into_db("INSERT INTO `person` (`prename`, `name`, `street`, `streetnumber`, `zip`) VALUES ('$prename', '$name', '$street', '$streetnumber', $zip)"); 
+			//Die ID des Eintrags wird als Kundennummer weiter gegeben
 			order_to_db($person_id);
 		}
 		function order_to_db($person_id){
+			// Fügt die Kundennummer, den Gesamtpreis der Bestellung und die Zahlungsart zur Tabelle `order` in der DB hinzu
 			$payment = $_POST['payment'];
 			foreach ($_SESSION['cart'] as $article){
 				$totalprice += ($article['price'] * $article['quantity']);
 			}
 			$order_nr = into_db("INSERT INTO `order`(`person_id`, `total_price`, `payment`) VALUES ('$person_id', $totalprice, '$payment')"); 
-			order_nr_to_db($order_nr);
+			//Die ID des Eintrags wird als Bestellnr weiter gegeben sowie die Kundennummer
+			order_nr_to_db($person_id, $order_nr);
 		}
-		function order_nr_to_db($order_nr){
+		function order_nr_to_db($person_id, $order_nr){
+			// Fügt die Bestellnr, Artikel, Anzahl der Artikel und Preis der gekauften Artikel für jeden Artikeltyp einzeln zur Tabelle `order_nr` in der DB hinzu
+			$payment = $_POST['payment'];
 			foreach ($_SESSION['cart'] as $article){
 				$price = $article['price'] * $article['quantity'];
 				$name = $article['name'];
 				$quantity = $article['quantity'];
 				$order_id = into_db("INSERT INTO `order_nr`(`order_nr`, `article`, `quantity`, `total_price_article`) VALUES ($order_nr, '$name', $quantity, $price)"); 
-			}	
+			}
+			end_session($person_id, $order_nr);	
 		}
 		function zip_to_db(){
+			// Fügt die PLZ zur Tabelle 'zip' in der DB hinzu
+			$payment = $_POST['payment'];
 			$zip = $_POST['zip'];
 			$city = $_POST['city'];
 			into_db("INSERT INTO `zip` (`zip`, `city) VALUES ($zip, '$city')"); 
 			
 		}
-		/*Vielen Dank & Bestellnummer & PersonenID ausgeben 
-		Bestellung noch mal anzeigen
-		Datenbank füllen
-		Emailadresse hinzufügen
-		Drop Session
-		*/
+		function end_session($person_id, $order_nr){
 		?>
-		<p>Vielen Dank für Ihre Bestellung!</p>
+			<p>Vielen Dank für Ihre Bestellung!</p>	</br>
+			<p>Ihre Bestellnummer lautet: <?=$order_nr?></p>
+			<p>Ihre Kundennummer lautet: <?=$person_id?></p>
+		<?php
+			//leert die Session für den Warenkorb
+			session_unset();
+		} ?>
+
 <?php
 	//Insert Footer
 	require_once 'inc/footer.php.inc';
